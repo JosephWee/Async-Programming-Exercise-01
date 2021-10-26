f0eaaab918d5f2dd76911c56d20891e33ec67a40
JosephWee <joseph.wee.tze.yeong@gmail.com>
2021-10-26 11:50:36 +0800

    Add MathWebApi01, MathWebApi02, MathWebApi03

---
+++ MathWebApi/Controllers/MathController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MathWebApi.Controllers
+{
+    public class MathController : ApiController
+    {
+        private List<Uri> FindPeers()
+        {
+            List<Uri> peerUris = new List<Uri>();
+
+            string peersAppSetting = System.Configuration.ConfigurationManager.AppSettings["peers"];
+            string[] peers = peersAppSetting.Split(' ');
+            foreach (var peer in peers)
+            {
+                Uri peerUri = null;
+                if (Uri.TryCreate(peer, UriKind.RelativeOrAbsolute, out peerUri))
+                {
+                    peerUris.Add(peerUri); 
+                }
+            }
+
+            return peerUris;
+        }
+
+        public IHttpActionResult GetSum([FromUri] int[] operand)
+        {
+            List<Uri> peers = FindPeers();
+
+            int result = operand.Length > 0 ? operand[0] : 0;
+
+            for (int i = 1; i < operand.Length; i++)
+            {
+                result += operand[i];
+            }
+
+            return Ok(result);
+        }
+    }
+}
